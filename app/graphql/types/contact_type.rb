9716bc73add32a4cde0c4df7eae0e4c49fd9e00d
tb <t.bak@selleo.com>
2017-10-07 17:21:10 +0200

    Add ContactInputType

--- app/graphql/types/contact_type.rb
+++ app/graphql/types/contact_type.rb
@@ -2,11 +2,20 @@ Types::ContactType = GraphQL::ObjectType.define do
   name "Contact"
 
   field :id, types.ID
-  field :errors, Types::JSONType
   field :last_name, types.String
   field :first_name, types.String
   field :email, types.String
   field :job_title, types.String
   field :business_phone, types.String
   field :errors, Types::JSONType
+end
+
+Types::ContactInputType = GraphQL::InputObjectType.define do
+  name "ContactInput"
+
+  argument :last_name, types.String
+  argument :first_name, types.String
+  argument :email, types.String
+  argument :job_title, types.String
+  argument :business_phone, types.String
 end
