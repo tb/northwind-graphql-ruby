10b1953408fe2276bdc634db526af79dbb3d6fd3
tb <t.bak@selleo.com>
2017-10-11 21:57:25 +0200

    Display all suppliers using apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,68 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+import { Table } from 'reactstrap';
+
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
+  render () {
+    const { loading, error, allSuppliers } = this.props.allSuppliers;
+
+    if (loading) {
+      return (<div>Loading</div>)
+    }
+
+    if (error) {
+      console.log(error);
+      return (<div>An unexpected error occurred</div>)
+    }
+
+    if (!allSuppliers) {
+      return (<div>No allSuppliers</div>)
+    }
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Northwind GraphQL Ruby Client</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Supplier Name</th>
+              <th>Contact</th>
+            </tr>
+          </thead>
+          <tbody>
+          {allSuppliers.map((supplier, index) =>
+            <tr key={index}>
+              <th scope="row">{index+1}</th>
+              <td>{supplier.name}</td>
+              <td>{JSON.stringify(supplier.contact, null, 2)}</td>
+            </tr>
+          )}
+          </tbody>
+        </Table>
       </div>
     );
   }
 }
 
-export default App;
+export const ALL_SUPPLIERS_QUERY = gql`
+  query allSuppliersQuery {
+    allSuppliers {
+      name
+      contact {
+        first_name
+        last_name
+        email
+      }
+    }
+  }
+`;
+
+export default graphql(ALL_SUPPLIERS_QUERY, { name: 'allSuppliers' })(App);
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const networkInterface = createNetworkInterface({
+  uri: '/graphql',
+});
+
+const client = new ApolloClient({
+  networkInterface,
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+      <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
