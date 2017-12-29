0a7807de0047f037ec67bccb27257d109a6227a9
johnunclesam <lexmealux@live.it>
2017-12-29 02:31:44 +0100

    Update contact.rb
    https://github.com/tb/northwind-graphql-ruby/issues/27

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ApplicationRecord
-  belongs_to :contactable, polymorphic: true
+  belongs_to :contactable, polymorphic: true, optional: true
 
   validates :first_name, :last_name, :email, presence: true
 end
