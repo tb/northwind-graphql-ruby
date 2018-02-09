b9511173384fee6043c4dafe0d85666a35db265b
tb <t.bak@selleo.com>
2018-02-09 23:17:32 +0100

    Use withData as component for table loaders

--- client/src/hocs/withData.js
+++ client/src/hocs/withData.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import {LoadingOverlay, Loader} from 'react-overlay-loader';
+import 'react-overlay-loader/styles.css';
 
 export const withData = WrappedComponent => props => {
-  const {error, loading} = props.data;
+  const {data, table, nodes = []} = props;
+  const {error, loading} = data;
+
+  if (table) {
+    if (loading) {
+      if (!nodes.length) {
+        return <div>Loading...</div>;
+      }
+
+      return (
+        <LoadingOverlay style={{minHeight: 100}}>
+          <Loader loading={loading} text={null} />
+          <WrappedComponent {...props} />
+        </LoadingOverlay>
+      );
+    }
+
+    if (!nodes.length) {
+      return <div>No data</div>;
+    }
+  }
 
   if (loading) {
-    return <div>Loading</div>;
+    return <div>Loading...</div>;
   }
 
   if (error) {
